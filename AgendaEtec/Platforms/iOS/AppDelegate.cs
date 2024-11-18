59de74a352e64cf3921c2f4e17e76c0b1b273fb3
gabil <gabil@SANTANA_BARROS>
2024-11-18 00:36:43 -0300

    Adicionar arquivos de projeto.

---
+++ AgendaEtec/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AgendaEtec;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
